a2c719d4d4ac29eee29d854abba137abfd1ce015
wojtek-baranowski <wojciech.jan.baranowski@gmail.com>
2016-11-04 19:50:02 +0100

    adding CMake suite

---
+++ build/CMakeFiles/Happy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Happy.dir/suite.cpp.o"
+  "Happy.pdb"
+  "Happy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Happy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ suite.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main() {
+
+std::cout<<"new app on my git hub project"<<std::endl;
+return 0;}
